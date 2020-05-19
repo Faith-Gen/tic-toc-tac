b72446546fb6eb7adfd46293b92ace29cfeb81c1
innoflash <innocentmazando@gmail.com>
2020-05-19 17:45:47 +0200

    added horizontal and vertical win checks

--- ttt/src/components/HelloWorld.vue
+++ ttt/src/components/HelloWorld.vue
@@ -32,14 +32,48 @@
                 ]
             }
         },
-      methods:{
-        updateBox(rowIndex, itemIndex, item){
-          if(item === '0' || item.length === 0)
-            this.items[rowIndex][itemIndex] = 'X'
-          else this.items[rowIndex][itemIndex] = '0'
+        methods: {
+            updateBox(rowIndex, itemIndex, item) {
+                if (item === '0' || item.length === 0)
+                    this.items[rowIndex][itemIndex] = 'X'
+                else this.items[rowIndex][itemIndex] = '0'
 
-          this.items = JSON.parse(JSON.stringify(this.items))
+                this.items = JSON.parse(JSON.stringify(this.items))
+
+                this.checkIfUserWon()
+            },
+            checkIfUserWon() {
+                let won = false
+                //horizontal check
+                if (this.checkRows(this.items)) {
+                    alert('You won!')
+                    return
+                }
+
+                //vertival check
+                let verticalArray = []
+                this.items.forEach((row, index) => {
+                    verticalArray.push([
+                        this.items[0][index],
+                        this.items[1][index],
+                        this.items[2][index],
+                    ])
+                })
+
+                if (this.checkRows(verticalArray)) {
+                    alert('You won!')
+                    return
+                }
+            },
+            checkRows(rows) {
+                let won = false
+                rows.forEach(row => {
+                    let uniqueValues = [...new Set(row)]
+                    if (uniqueValues.length === 1 && (uniqueValues[0] === 'X' || uniqueValues[0] === '0'))
+                        won = true
+                })
+                return won
+            }
         }
-      }
     }
 </script>
