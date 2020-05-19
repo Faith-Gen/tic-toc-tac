c2a1905c22d1edae94ec1f609556cbc81c474893
innoflash <innocentmazando@gmail.com>
2020-05-19 17:28:45 +0200

    swapping numbers

--- ttt/src/components/HelloWorld.vue
+++ ttt/src/components/HelloWorld.vue
@@ -1,58 +1,45 @@
 <template>
-  <div>
-    <div id="root">
+    <div>
+        <div id="root">
 
+        </div>
+
+        <div class="status">
+            Next player: X
+        </div>
+        <div class="board">
+            <div class="board-row first-row" v-for="(row, rowIndex) in items">
+                <button class="square" v-for="(item, itemIndex) in row" @click="updateBox(rowIndex, itemIndex, item)">
+                    {{ item }}
+                </button>
+            </div>
+        </div>
     </div>
-
-    <div class="status">
-      Next player: X
-    </div>
-    <div class="board">
-      <div class="board-row first-row">
-        <button class="square">
-          X
-        </button>
-        <button class="square">
-
-        </button>
-        <button class="square">
-          O
-        </button>
-      </div>
-
-      <div class="board-row second-row">
-        <button class="square">
-
-        </button>
-        <button class="square">
-
-        </button>
-        <button class="square">
-
-        </button>
-      </div>
-
-      <div class="board-row third-row">
-        <button class="square">
-
-        </button>
-        <button class="square">
-
-        </button>
-        <button class="square">
-
-        </button>
-      </div>
-    </div>
-
-  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                items: [
+                    ['X', '', ''],
+                    ['', '', ''],
+                    ['', '', ''],
+                ]
+            }
+        },
+      methods:{
+        updateBox(rowIndex, itemIndex, item){
+          if(item === '0' || item.length === 0)
+            this.items[rowIndex][itemIndex] = 'X'
+          else this.items[rowIndex][itemIndex] = '0'
+
+          this.items = JSON.parse(JSON.stringify(this.items))
+        }
+      }
+    }
 </script>
