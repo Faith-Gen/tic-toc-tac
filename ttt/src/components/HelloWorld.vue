4fc55c65236c84d3e7ba99653a3276bceca0aac9
innoflash <innocentmazando@gmail.com>
2020-05-19 17:16:37 +0200

    added vue

---
+++ ttt/src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div id="root">
+
+    </div>
+
+    <div class="status">
+      Next player: X
+    </div>
+    <div class="board">
+      <div class="board-row first-row">
+        <button class="square">
+          X
+        </button>
+        <button class="square">
+
+        </button>
+        <button class="square">
+          O
+        </button>
+      </div>
+
+      <div class="board-row second-row">
+        <button class="square">
+
+        </button>
+        <button class="square">
+
+        </button>
+        <button class="square">
+
+        </button>
+      </div>
+
+      <div class="board-row third-row">
+        <button class="square">
+
+        </button>
+        <button class="square">
+
+        </button>
+        <button class="square">
+
+        </button>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
